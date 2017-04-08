cd88e5b89990b23c26a0ee665d5f1522c45f27e7
slava hatnuke <slava.hatnuke@gmail.com>
2017-04-08 04:04:10 +0300

    ^

---
+++ README.md
@@ -0,0 +1,13 @@
+## iReactivity simple react native example
+The main idea:
+
+```javascript
+
+```
+
+## How to start
+- `npm install`
+- `npm start`
+
+## iReactivity
+[https://www.npmjs.com/package/ireactivity](https://www.npmjs.com/package/ireactivity) - Simple React binding 
--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  Text,
-  View
+    AppRegistry,
+    Text,
+    View,
+    Button
 } from 'react-native';
+
+// iReactivity binding functions
+import {Provider, connect} from 'ireactivity';
 
 //
 // export default class ireactivityNativeSimpleListExample extends Component {
@@ -45,9 +49,67 @@ import {
 // });
 
 
+const uid = () => Math.random().toString(35).slice(2, 8).toUpperCase();
+
+const store = {
+    todos: [
+        {title: 'Todo #1', id: uid()},
+        {title: 'Todo #2', id: uid()}
+    ]
+};
+
+const TodoView = ({todo, onRemove}) =>
+    <View>
+        <Text>{todo.title}</Text>
+
+        <Button
+            onPress={() => onRemove(todo)}
+            title={`[x]`}
+            color="#FF0000"
+        />
+    </View>
+
+const Todo = connect(TodoView, {
+    onRemove: (store) => (todo) => {
+        console.log('remove', todo.id);
+        store.todos = store.todos.filter((aTodo) => todo !== aTodo)
+    }
+});
+
+const TodosView = ({todos}) =>
+    <View>
+        {todos.map((todo) => <Todo key={'' + todo.id} todo={todo}/>)}
+    </View>;
+
+const Todos = connect(TodosView, {
+    todos: (store) => store.todos
+});
+
+const TodoPlusView = ({onClick}) =>
+<Button
+    onPress={onClick}
+    title="Add"
+    color="#000000"
+/>
+
+const TodoPlus = connect(TodoPlusView, {
+    onClick: (store) => () => {
+        let id = uid();
+        store.todos = [...store.todos, {title: `Todo #${id}`, id: id}]
+    }
+});
+
+// const AppView = () => <View><Text>List</Text> <TodoPlus/> <Todos/></View>;
+
+// const App = AppView;
 
 
+// <View><Text>List</Text> <TodoPlus/> <Todos/></View>
+const App = () =>
+    <View style={ {alignItems: 'center', margin: 20} }>
+        <TodoPlus/>
+        <Todos/>
+    </View>;
 
-const App = () => <View style={ {alignItems: 'center', margin: 20} }><Text>OK</Text></View>;
-
-AppRegistry.registerComponent('ireactivityNativeSimpleListExample', () => App);
+const AppRunner = () => <Provider store={store}><App/></Provider>;
+AppRegistry.registerComponent('ireactivityNativeSimpleListExample', () => AppRunner);
